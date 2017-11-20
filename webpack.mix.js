f3b1fd7fc60bd967448a505bb340f2442e65829f
Rathes Sachchithananthan <rathes@aheenam.com>
2017-11-20 14:32:35 +0100

    Removes Bulma and adds Tailwind instead

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 let webpack = require('webpack');
+let tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ let webpack = require('webpack');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind-config.js') ],
+    })
     .sourceMaps()
     .version();
 
