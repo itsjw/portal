a9e6dae198ca71640db531cb8b6bd3572a8ab599
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:14:34 +0100

    Added Meetup Views

---
+++ resources/views/meetups/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+
+@section('page_title', 'Create Meetup')
+
+@push('header_scripts')
+
+@endpush
+
+@push('styles')
+
+@endpush
+
+@section('content')
+
+@endsection
+
+@push('scripts')
+
+@endpush
---
+++ resources/views/meetups/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+
+@section('page_title', 'Edit Meetup')
+
+@push('header_scripts')
+
+@endpush
+
+@push('styles')
+
+@endpush
+
+@section('content')
+
+@endsection
+
+@push('scripts')
+
+@endpush
