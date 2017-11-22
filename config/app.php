58cf07bc1769f628d5fad7ac196bc1d53dd4ea72
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:03:45 +0100

    Added CashierServiceProvider

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Cashier\CashierServiceProvider::class,
 
         /*
          * Application Service Providers...
