f5713330931487954c86e4632bb33b1eeac581bd
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:08:34 +0100

    Added new routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,28 @@
 <?php
 
+/*
+ * User Routes
+ */
 Route::apiResource('users', 'Api\Users\UserController');
-
 Route::post('users/{user}/avatar', 'Api\Users\UserAvatarController@store')->name('avatar');
+
+/*
+ * Job Routes
+ */
+Route::apiResource('jobs', 'Api\Jobs\JobController');
+
+/*
+ * Meetup Routes
+ */
+Route::apiResource('meetups', 'Api\Meetups\MeetupController');
+
+/*
+ * Link Routes
+ */
+Route::apiResource('links', 'Api\Links\LinkController');
+Route::apiResource('link-categories', 'Api\Links\LinkCategoryController');
+
+/*
+ * Discount Routes
+ */
+Route::apiResource('discounts', 'Api\Discounts\DiscountController');
