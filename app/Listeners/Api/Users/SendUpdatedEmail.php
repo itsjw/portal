0d8f544172137025ea6f1f44b28508c728500f18
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:14:01 +0100

    Added API Event Listeners

---
+++ app/Listeners/Api/Users/SendUpdatedEmail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners\Api\Users;
+
+use App\Events\Api\Users\UserUpdated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendUpdatedEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserUpdated  $event
+     * @return void
+     */
+    public function handle(UserUpdated $event)
+    {
+        //
+    }
+}
