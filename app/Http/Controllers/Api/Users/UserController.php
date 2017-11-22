12bdbbd3e140b95ff6b49a71a03fa06f845c63fd
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 07:58:22 +0100

    Update on UserController

--- app/Http/Controllers/Api/Users/UserController.php
+++ app/Http/Controllers/Api/Users/UserController.php
@@ -2,93 +2,113 @@
 
 namespace App\Http\Controllers\Api\Users;
 
-use App\Http\Controllers\Controller;
+use App\Events\Api\Users\UserCreated;
+use App\Events\Api\Users\UserUpdated;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection|\Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::all();
+        $perPage = $request->get('per_page') ?? '15';
+
+        $users = User::paginate($perPage);
 
         return UserResource::collection($users);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user = new User();
+        $this->authorize('store');
+
+        $created = $user->create([
+            'name'               => $request->get('name'),
+            'email'              => $request->get('email'),
+            'username'           => str_slug($request->get('username'), '_'),
+            'confirmation_token' => str_random(64),
+            'password'           => str_random(12),
+            'lat'                => $request->get('lat'),
+            'lng'                => $request->get('lng'),
+            'city'               => $request->get('city'),
+            'country'            => $request->get('country'),
+            'affiliate_id'       => str_random(10),
+        ]);
+
+        event(new UserCreated($created));
+
+        return UserResource::make($created);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param int $id
-     *
+     * @param $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
-    }
+        $user = User::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return UserResource::make($user);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int                      $id
-     *
+     * @param  \Illuminate\Http\Request $request
+     * @param $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        $this->authorize('update', $user);
+
+        $user->update([
+            'name'               => $request->get('name'),
+            'email'              => $request->get('email'),
+            'username'           => str_slug($request->get('username'), '_'),
+            'lat'                => $request->get('lat'),
+            'lng'                => $request->get('lng'),
+            'city'               => $request->get('city'),
+            'country'            => $request->get('country'),
+            'affiliate_id'       => str_random(10),
+        ]);
+
+        event(new UserUpdated($user));
+
+        return UserResource::make($user);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
-     *
+     * @param $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $this->authorize('delete', $user);
+
+        $user->delete();
+
+        return UserResource::make($user);
     }
 }
