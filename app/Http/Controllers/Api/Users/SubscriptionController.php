ecae640d39b1649d521e5c5c71f688b1b03010f8
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:05:42 +0100

    Started work on Subscriptions

---
+++ app/Http/Controllers/Api/Users/SubscriptionController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api\Users;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SubscriptionController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return static
+     */
+    public function createSubscription(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+
+        $user->newSubscription('main', $request->get('plan'))->create($request->get('stripeToken'), [
+            'email' => $request->get('email'),
+        ]);
+
+        return UserResource::make($user);
+    }
+
+    /**
+     * @return static
+     */
+    public function cancelSubscription()
+    {
+        $user = User::find(auth()->user()->id);
+
+        $user->subscription('main')->cancelNow();
+
+        return UserResource::make($user);
+    }
+
+    /**
+     * @param Request $request
+     * @return static
+     */
+    public function updateCard(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+
+        $user->updateCard($request->get('stripeToken'));
+
+        return UserResource::make($user);
+    }
+}
