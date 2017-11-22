a16b431f2ce18c7c266bf0f50ff98b7ce84e02e5
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:10:31 +0100

    Working on Discounts

---
+++ resources/views/discounts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+
+@section('page_title', 'Create Discount')
+
+@push('header_scripts')
+
+@endpush
+
+@push('styles')
+
+@endpush
+
+@section('content')
+
+@endsection
+
+@push('scripts')
+
+@endpush
