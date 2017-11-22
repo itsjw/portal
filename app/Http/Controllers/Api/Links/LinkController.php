3b3a1026597484b0516f3078995d4d0cdc701b6e
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-22 08:12:22 +0100

    Added new API Controller

---
+++ app/Http/Controllers/Api/Links/LinkController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api\Links;
+
+use App\Http\Resources\LinkResource;
+use App\Models\Link;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection|\Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->get('per_page') ?? '15';
+
+        $links = Link::with('category', 'author')->paginate($perPage);
+
+        return LinkResource::collection($links);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $created = $request->user()->links->create([
+            'title' => $request->get('title'),
+            'url' => $request->get('url'),
+            'link_category_id' => $request->get('category_id')
+        ]);
+
+        return LinkResource::make($created);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $link = Link::find($id)->with('category', 'author');
+
+        return LinkResource::make($link);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $link = Link::find($id);
+
+        $updated = $link->update([
+            'title' => $request->get('title'),
+            'url' => $request->get('url'),
+            'link_category_id' => $request->get('category_id')
+        ]);
+
+        return LinkResource::make($updated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $link = Link::find($id);
+
+        $link->delete();
+
+        return LinkResource::make($link);
+    }
+}
---
+++ app/Http/Controllers/Api/Meetups/MeetupController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api\Meetups;
+
+use App\Models\Meetup;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MeetupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Meetup  $meetup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meetup $meetup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Meetup  $meetup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meetup $meetup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Meetup  $meetup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Meetup $meetup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Meetup  $meetup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meetup $meetup)
+    {
+        //
+    }
+}
