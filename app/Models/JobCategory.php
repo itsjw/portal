190bf7bc6bcf86d5777b54cb9166190daf95a2ab
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-23 13:49:23 +0100

    Added relation and trait

--- app/Models/JobCategory.php
+++ app/Models/JobCategory.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class JobCategory extends Model
 {
-    //
+    use Searchable;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function jobs()
+    {
+        return $this->hasMany(Job::class, 'job_category_id', 'id');
+    }
 }
