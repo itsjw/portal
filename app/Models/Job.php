3a83830487c418188de7f6248f8680b726c9d244
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-23 13:49:10 +0100

    Added relations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Traits\RecordsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Job extends Model
 {
-    //
+    use Searchable, RecordsActivity;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(JobCategory::class, 'job_category_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company()
+    {
+        return $this->belongsTo(JobCompany::class, 'job_company_id', 'id');
+    }
 }
