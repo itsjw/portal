1acacde765eb8d0265e1303a81d2e629c546a099
Florian Wartner <fwartner@users.noreply.github.com>
2017-11-30 08:14:01 +0100

    Apply fixes from StyleCI (#9)
    [ci skip] [skip ci]

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -5,10 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Job::class, function (Faker $faker) {
     return [
         'job_category_id' => rand(1, 6),
-        'job_company_id' => rand(1, 10),
-        'title' => $faker->jobTitle,
-        'description' => $faker->paragraphs(3, true),
-        'perks' => $faker->paragraphs(2, true),
-        'expectation' => $faker->paragraphs(2, true),
+        'job_company_id'  => rand(1, 10),
+        'title'           => $faker->jobTitle,
+        'description'     => $faker->paragraphs(3, true),
+        'perks'           => $faker->paragraphs(2, true),
+        'expectation'     => $faker->paragraphs(2, true),
     ];
 });
--- database/factories/MeetupFactory.php
+++ database/factories/MeetupFactory.php
@@ -6,16 +6,16 @@ $factory->define(App\Models\Meetup::class, function (Faker $faker) {
     $url_name = $faker->url;
 
     return [
-        'name' => $faker->domainName,
-        'link' => $url_name,
-        'url_name' => $url_name,
-        'description' => $faker->paragraphs(3, true),
+        'name'                   => $faker->domainName,
+        'link'                   => $url_name,
+        'url_name'               => $url_name,
+        'description'            => $faker->paragraphs(3, true),
         'localized_country_name' => $faker->country,
-        'state' => $faker->countryCode,
-        'lat' => $faker->latitude,
-        'lon' => $faker->longitude,
-        'member_count' => \App\Models\User::count(),
-        'who' => rand(1, 100),
-        'timezone' => $faker->timezone
+        'state'                  => $faker->countryCode,
+        'lat'                    => $faker->latitude,
+        'lon'                    => $faker->longitude,
+        'member_count'           => \App\Models\User::count(),
+        'who'                    => rand(1, 100),
+        'timezone'               => $faker->timezone,
     ];
 });
