49c90e7b67f6cd72b1ae43726a7a10f17318d8a3
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-30 07:44:00 +0100

    Added pagination

--- app/Http/Controllers/Web/Jobs/JobController.php
+++ app/Http/Controllers/Web/Jobs/JobController.php
@@ -23,7 +23,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all();
+        $jobs = Job::paginate(15);
 
         return view('jobs.index', compact('jobs'));
     }
