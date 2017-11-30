a795acde9b96ad289d4eb72c0ab9f9a9e27e2b8d
Florian Wartner <florian.wartner@aboutyou.de>
2017-11-30 07:20:01 +0100

    Added LinkFactory for development

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Link::class, function (Faker $faker) {
+    return [
+        'user_id' => rand(1, 100),
+        'link_category_id' => rand(1, 6),
+        'title' => 'Velox, superbus guttuss inciviliter.',
+        'url' => $faker->url
+    ];
+});
